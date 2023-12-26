3547458d99249c376333e8dd44647d32bd58dfdf
juno <kraksin12@gmail.com>
2023-12-26 10:15:44 +0900

    feat: main 브랜치 테스트

--- packages/utils/src/getPriceWithComma.ts
+++ packages/utils/src/getPriceWithComma.ts
@@ -8,6 +8,7 @@ export function getPriceWithComma({
   currency = 'ko-KR',
 }: GetPriceWithCommaProps): string {
   const intNum = typeof price === 'string' ? parseInt(price, 10) : price;
+  console.log('Main Branch Test');
 
   if (Number.isNaN(intNum)) {
     return '';
